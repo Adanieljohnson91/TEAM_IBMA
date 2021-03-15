2ed92459603afe2c77bcee218b4a58903844401e
AdanielJohnson91 <dan.johnson1022@gmail.com>
2021-03-15 16:43:21 -0500

    userServices template

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const userRoutes = require('./routes/userRoutes');
+
+app.use("/user", userRoutes);
+
+
+app.listen(8080, ()=>{
+    console.log("Listening on port 8080")
+});
--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -1,1 +1,12 @@
-const UserSchema = require("./")
+const { create } = require("../models/UserSchema")
+const User = require("../models/UserSchema")
+
+async function create(user){
+    let newUser = new User(user);
+    let doc = await newUser.save();
+    return doc;
+}
+
+module.exports = {
+    create
+}
